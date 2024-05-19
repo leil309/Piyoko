133f70981d0cf8bfc87c1d39ee8f5b30d48bf774
이일우 <leil309@naver.com>
2024-05-19 22:53:06 +0900

    [master] Collision 테스트, 플레이어 이동 수정, AutotileSet 수정

--- Character/Player/Player.cs
+++ Character/Player/Player.cs
@@ -3,7 +3,13 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export] public int Speed { get; set; } = 300;
+	[ExportCategory("Movement")]
+	[Export]
+	public int MaxSpeed { get; set; } = 75; //최대 속도
+	[Export]
+	public int Acceleration = 100; //가속 - 낮을수록 최대속도에 도달하기까지 오래걸림
+	[Export]
+	public int Friction { get; set; } = 100; //마찰 - 낮을수록 천천히 멈춤
 
 	public override void _Ready()
 	{
@@ -12,14 +18,20 @@ public partial class Player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		var velocity = Vector2.Zero;
-		velocity.X = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-		velocity.Y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
-		if (velocity.Length() > 0)
+		float fDelta = (float)delta;
+		Vector2 inputVector = Vector2.Zero;
+		inputVector.X = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+		inputVector.Y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+		inputVector = inputVector.Normalized();
+
+		if (inputVector != Vector2.Zero)
 		{
-			velocity = velocity.Normalized() * Speed;
+			Velocity = Velocity.MoveToward(MaxSpeed * inputVector, (Acceleration* (Acceleration/2)) * fDelta);
 		}
-		Velocity = velocity;
+		else
+		{
+			Velocity = Velocity.MoveToward(Vector2.Zero, (Friction* (Friction/2)) * fDelta);
+		}
 		MoveAndSlide();
 	}
 
