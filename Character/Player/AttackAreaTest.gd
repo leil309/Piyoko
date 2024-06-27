4a5429da30a295ce7565eb855331af8a40ee2490
이일우 <leil309@naver.com>
2024-06-27 15:37:04 +0900

    [master] enemy 체력, 데미지 테스트

---
+++ Character/Player/AttackAreaTest.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+var attack_damage = 10.5
+var knockback_force = 100.0
+
+func _on_hitbox_area_entered(area):
+	if area.has_method("damage"):
+		print("ATTACK!")
+		var attack = Attack.new()
+		attack.attack_damage = attack_damage
+		attack.knockback_force = knockback_force
+		attack.attack_position = global_position
+		area.damage(attack)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	pass
+
+func _on_area_entered(area):
+	_on_hitbox_area_entered(area)
